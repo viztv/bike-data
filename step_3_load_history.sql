57bf2ba3e249156bd855a68e99dd4ecd655b9c67
Troy Green <troygreen99@yahoo.com>
2015-08-09 16:47:55 -0700

    Delete step_3_load_history.sql

--- step_3_load_history.sql
+++
@@ -1,3 +0,0 @@
-truncate raw_data;
-
-LOAD DATA LOCAL INFILE 'bike_data/raw_data.csv' INTO TABLE raw_data FIELDS TERMINATED BY ',' LINES TERMINATED BY '\r\n' ignore 1 lines (tripduration,starttime,stoptime,start_station_id,start_station_name,start_station_latitude,start_station_longitude,end_station_id,end_station_name,end_station_latitude,end_station_longitude,bikeid,usertype,birth_year,gender);
